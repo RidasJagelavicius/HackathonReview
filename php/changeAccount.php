eed1b633ac19d287868e8cc4065165e5b87907f7
Matas Lauzadis <matas.lauzadis@gmail.com>
2019-03-10 09:35:59 -0500

    Removed database connection

--- php/changeAccount.php
+++ php/changeAccount.php
@@ -3,12 +3,6 @@
 include 'functions.php';
 session_start();
 
-// Login information
-$dbhost = 'localhost';
-$dbuser = 'root';
-$dbpass = 'raspberrypi';
-$db = 'HackathonReview';
-
 // Create connection to SQL
 $conn = mysqli_connect($dbhost, $dbuser, $dbpass, $db);
 
