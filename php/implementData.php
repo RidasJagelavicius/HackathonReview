28553dcd85b01616667931ef7994392bd5e7d257
mataslauzadis <30608308+mataslauzadis@users.noreply.github.com>
2019-03-13 16:07:06 -0500

    Update implementData.php

--- php/implementData.php
+++ php/implementData.php
@@ -6,34 +6,34 @@ $file = fopen('HackathonInfo.txt','r');
 
 $count = 1;
 
-$name = array(); $state; $city; $start; $end; $siteURL; $logoURL; $backgroundURL;
+$name=array(); 
+$state=array(); 
+$city=array(); 
+$start=array(); 
+$end=array(); 
+$siteURL=array(); 
+$logoURL=array(); 
+$backgroundURL=array();
 
 while(!feof($file))
 {
 	if($count%9 == 1)
 		$name[] = fgets($file);
 	else if($count%9 == 2)
-		$start = fgets($file);
+		$start[] = fgets($file);
 	else if($count%9 == 3)
-		$end = fgets($file);
+		$end[] = fgets($file);
 	else if($count%9 == 4)
-		$city = fgets($file);
+		$city[] = fgets($file);
 	else if($count%9 == 5)
-		$state = fgets($file);
+		$state[] = fgets($file);
 	else if($count%9 == 6)
-		$siteURL = fgets($file);
+		$siteURL[] = fgets($file);
 	else if($count%9 == 7)
-		$logoURL = fgets($file);
+		$logoURL[] = fgets($file);
 	else if($count%9 == 8)
-		$backgroundURL = fgets($file);
+		$backgroundURL[] = fgets($file);
 	$count++;
 }
-
 var_dump($name);
-
-for($x = 0; $x < $count; $x++)
-{
-	echo $name[$x] . '<br>';
-}
-
 ?>
